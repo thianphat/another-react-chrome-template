4158f52cced86033eefbfa9a12fc47a663233920
thianphat <bright.s.williams@gmail.com>
2022-10-27 20:42:57 -0700

    base boilerplate

---
+++ src/popup/popup.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './popup.css'
+
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+        <h1>Omg</h1>
+      <img src="icon.png" />
+    </div>
+  )
+}
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+ReactDOM.render(<App />, root)
